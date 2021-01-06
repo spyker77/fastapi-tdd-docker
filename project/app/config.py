414394f18860e7647f96f8e3e55c8a72df3348d1
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-01-06 15:39:22 +0300

    Initial commit

---
+++ project/app/config.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv("ENVIRONMENT", "dev")
+    testing: bool = bool(os.getenv("TESTING", 0))
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
---
+++ project/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, FastAPI
+
+from app.config import Settings, get_settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
