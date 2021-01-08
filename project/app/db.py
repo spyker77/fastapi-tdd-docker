95e0a63e6ae03862e8127e9ddb19a53784293fd0
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-01-08 20:32:03 +0300

    Use config instead of (db_url, modules)

--- project/app/db.py
+++ project/app/db.py
@@ -8,45 +8,35 @@ from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
 
 PARSED_DB_URL = urlparse(os.environ.get("DATABASE_URL"))
+DB_CREDENTIALS = re.split(":|@", str(PARSED_DB_URL.netloc)) + [
+    str(PARSED_DB_URL.path).lstrip("/")
+]
 
-# Check DB type to avoid errors caused by incorrect parsing of credentials
-if "sqlite" in PARSED_DB_URL:
-    TORTOISE_ORM = {
-        "connections": {"default": os.environ.get("DATABASE_URL")},
-        "apps": {
-            "models": {
-                "models": ["app.models.tortoise"],
-                "default_connection": "default",
+
+# Helper for migrations: https://github.com/testdrivenio/fastapi-tortoise-aerich
+TORTOISE_ORM = {
+    "connections": {
+        "default": os.environ.get("DATABASE_URL")
+        if "sqlite" in PARSED_DB_URL
+        else {
+            "engine": "tortoise.backends.asyncpg",
+            "credentials": {
+                "user": DB_CREDENTIALS[0],
+                "password": DB_CREDENTIALS[1],
+                "host": DB_CREDENTIALS[2],
+                "port": DB_CREDENTIALS[3],
+                "database": DB_CREDENTIALS[4],
+                "ssl": "require" if os.environ.get("ENVIRONMENT") == "prod" else False,
             },
+        }
+    },
+    "apps": {
+        "models": {
+            "models": ["app.models.tortoise", "aerich.models"],
+            "default_connection": "default",
         },
-    }
-else:
-    DB_CREDENTIALS = re.split(":|@", str(PARSED_DB_URL.netloc)) + [
-        str(PARSED_DB_URL.path).lstrip("/")
-    ]
-    TORTOISE_ORM = {
-        "connections": {
-            "default": {
-                "engine": "tortoise.backends.asyncpg",
-                "credentials": {
-                    "user": DB_CREDENTIALS[0],
-                    "password": DB_CREDENTIALS[1],
-                    "host": DB_CREDENTIALS[2],
-                    "port": DB_CREDENTIALS[3],
-                    "database": DB_CREDENTIALS[4],
-                    "ssl": "require"
-                    if os.environ.get("ENVIRONMENT") == "prod"
-                    else False,
-                },
-            },
-        },
-        "apps": {
-            "models": {
-                "models": ["app.models.tortoise"],
-                "default_connection": "default",
-            },
-        },
-    }
+    },
+}
 
 
 log = logging.getLogger("uvicorn")
@@ -55,7 +45,31 @@ log = logging.getLogger("uvicorn")
 def init_db(app: FastAPI) -> None:
     register_tortoise(
         app,
-        config=TORTOISE_ORM,
+        config={
+            "connections": {
+                "default": os.environ.get("DATABASE_URL")
+                if "sqlite" in PARSED_DB_URL
+                else {
+                    "engine": "tortoise.backends.asyncpg",
+                    "credentials": {
+                        "user": DB_CREDENTIALS[0],
+                        "password": DB_CREDENTIALS[1],
+                        "host": DB_CREDENTIALS[2],
+                        "port": DB_CREDENTIALS[3],
+                        "database": DB_CREDENTIALS[4],
+                        "ssl": "require"
+                        if os.environ.get("ENVIRONMENT") == "prod"
+                        else False,
+                    },
+                }
+            },
+            "apps": {
+                "models": {
+                    "models": ["app.models.tortoise"],
+                    "default_connection": "default",
+                },
+            },
+        },
         generate_schemas=False,
         add_exception_handlers=True,
     )
@@ -63,7 +77,33 @@ def init_db(app: FastAPI) -> None:
 
 async def generate_schema() -> None:
     log.info("Initializing Tortoise...")
-    await Tortoise.init(config=TORTOISE_ORM)
+    await Tortoise.init(
+        config={
+            "connections": {
+                "default": os.environ.get("DATABASE_URL")
+                if "sqlite" in PARSED_DB_URL
+                else {
+                    "engine": "tortoise.backends.asyncpg",
+                    "credentials": {
+                        "user": DB_CREDENTIALS[0],
+                        "password": DB_CREDENTIALS[1],
+                        "host": DB_CREDENTIALS[2],
+                        "port": DB_CREDENTIALS[3],
+                        "database": DB_CREDENTIALS[4],
+                        "ssl": "require"
+                        if os.environ.get("ENVIRONMENT") == "prod"
+                        else False,
+                    },
+                }
+            },
+            "apps": {
+                "models": {
+                    "models": ["models.tortoise"],
+                    "default_connection": "default",
+                },
+            },
+        }
+    )
     log.info("Generating database schema via Tortoise...")
     await Tortoise.generate_schemas()
     await Tortoise.close_connections()
