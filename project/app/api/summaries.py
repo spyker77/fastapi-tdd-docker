fdba84ad840176c7f7bd1678ecbb281578872426
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-01-10 13:57:00 +0300

    Make isort use black's profile

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,9 +3,11 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema  # isort:split
-from app.models.pydantic import (SummaryResponseSchema,
-                                 SummaryUpdatePayloadSchema)
+from app.models.pydantic import (
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 from app.models.tortoise import SummarySchema
 
 router = APIRouter()
