5f5dcf7696a882928b37928c65017fb767fd22a3
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-01-11 18:39:05 +0300

    Add machine learning summarizer

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
 
 from app.api import crud
 from app.models.pydantic import (
@@ -9,13 +9,17 @@ from app.models.pydantic import (
     SummaryUpdatePayloadSchema,
 )
 from app.models.tortoise import SummarySchema
+from app.summarizer import generate_summary
 
 router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+async def create_summary(
+    payload: SummaryPayloadSchema, background_tasks: BackgroundTasks
+) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
+    background_tasks.add_task(generate_summary, summary_id, payload.url)
     response_object = SummaryResponseSchema(id=summary_id, url=payload.url)
     return response_object
 
