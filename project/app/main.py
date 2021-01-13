60b541aa4b9c2bbc8b01f47509c1e25c01f74145
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-01-13 19:53:32 +0300

    Add CORSMiddleware

--- project/app/main.py
+++ project/app/main.py
@@ -1,15 +1,30 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
 
+origins = [
+    "http://guarded-waters-54698.herokuapp.com",
+    "https://guarded-waters-54698.herokuapp.com",
+    "http://localhost",
+    "https://localhost",
+]
+
 
 def create_application() -> FastAPI:
     application = FastAPI()
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     application.include_router(ping.router)
     application.include_router(
         summaries.router, prefix="/summaries", tags=["summaries"]
