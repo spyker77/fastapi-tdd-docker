4ef526fc5ad053f088ec3f28b67c3f7e4c9de0e6
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-05-25 20:07:48 +0300

    Adapted pipeline to use 2 separate images for web and worker

--- release.sh
+++ release.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_IMAGE} --format={{.Id}})
-PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}, {"type": "worker", "docker_image": "'"$IMAGE_ID"'"}]}'
+WEB_IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_WEB_IMAGE} --format={{.Id}})
+WORKER_IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_WORKER_IMAGE} --format={{.Id}})
+PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$WEB_IMAGE_ID"'"}, {"type": "worker", "docker_image": "'"$WORKER_IMAGE_ID"'"}]}'
 
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
   -d "${PAYLOAD}" \
