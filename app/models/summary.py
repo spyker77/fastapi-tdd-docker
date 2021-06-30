48c285d7014cc621fe3fe716ed13246bceddf7d7
Evgeni Sautin <evgeni.sautin@gmail.com>
2021-06-30 15:58:38 +0300

    Update import path in models

--- app/models/summary.py
+++ app/models/summary.py
@@ -1,11 +1,12 @@
-from tortoise import fields, models
+from tortoise.fields import data
+from tortoise.models import Model
 
 
-class TextSummary(models.Model):
-    id = fields.IntField(pk=True)
-    url = fields.TextField()
-    summary = fields.TextField()
-    created_at = fields.DatetimeField(auto_now_add=True)
+class TextSummary(Model):
+    id = data.IntField(pk=True)
+    url = data.TextField()
+    summary = data.TextField()
+    created_at = data.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
