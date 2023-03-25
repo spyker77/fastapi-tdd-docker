a87c59377c0f1649add5ab7c75de8a24ebc31e15
Evgeni Sautin <evgeni.sautin@gmail.com>
2023-03-25 16:19:39 +0300

    Upgrade dependencies

--- app/tests/api/v2/test_ping_endpoint.py
+++ app/tests/api/v2/test_ping_endpoint.py
@@ -2,6 +2,6 @@ from app.main import app
 
 
 def test_ping(test_client_with_db):
-    response = test_client_with_db.get(app.url_path_for(name="pong"))
+    response = test_client_with_db.get(app.url_path_for("pong"))
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!", "testing": True, "environment": "dev"}
