072feaf706b66d5063fdee23a45c8171df83f286
Evgeni Sautin <evgeni.sautin@gmail.com>
2023-05-01 20:17:35 +0300

    Add type annotation to the models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,8 @@
+from typing import List
 from uuid import uuid4
 
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String, func
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Boolean, DateTime, ForeignKey, String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy_utils import UUIDType
 
 
@@ -10,24 +11,35 @@ class Base(DeclarativeBase):
 
 
 class TimestampMixin:
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    modified_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
 
 
 class AbstractBaseModel(TimestampMixin, Base):
     __abstract__ = True
 
-    id = Column(UUIDType(binary=False), primary_key=True, default=uuid4, unique=True, nullable=False, index=True)
+    id: Mapped[UUIDType] = mapped_column(
+        UUIDType(binary=False),
+        primary_key=True,
+        default=uuid4,
+        unique=True,
+        nullable=False,
+        index=True,
+    )
 
 
 class Summary(AbstractBaseModel):
     __tablename__ = "summaries"
 
-    url = Column(String)
-    summary = Column(String)
-    user_id = Column(UUIDType(binary=False), ForeignKey("users.id"))
+    url: Mapped[String] = mapped_column(String, index=True)
+    summary: Mapped[String] = mapped_column(String)
+    user_id: Mapped[UUIDType] = mapped_column(UUIDType(binary=False), ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="summaries")
+    user: Mapped["User"] = relationship("User", back_populates="summaries")
 
     def __str__(self):
         return self.url
@@ -36,14 +48,14 @@ class Summary(AbstractBaseModel):
 class User(AbstractBaseModel):
     __tablename__ = "users"
 
-    username = Column(String(255), unique=True, nullable=False, index=True)
-    email = Column(String(255), unique=True, nullable=False, index=True)
-    full_name = Column(String)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
-    hashed_password = Column(String)
+    username: Mapped[String] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    email: Mapped[String] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    full_name: Mapped[String] = mapped_column(String)
+    is_active: Mapped[Boolean] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[Boolean] = mapped_column(Boolean, default=False)
+    hashed_password: Mapped[String] = mapped_column(String)
 
-    summaries = relationship("Summary", back_populates="user")
+    summaries: Mapped[List["Summary"]] = relationship("Summary", back_populates="user")
 
     def __str__(self):
         return self.username
